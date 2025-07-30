3822bc2101a4ca9833a7355b752721811011e36b
김희망 <gimalgosa@gmail.com>
2025-07-30 16:43:00 +0900

    init

---
+++ mealplan/Form1.cs
@@ -0,0 +1,68 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mealplan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 로그인 폼 진입 시 
+        private void loginForm_Enter(object sender, EventArgs e)
+        {
+            TextBox loginForm = sender as TextBox;
+
+            // 로그인 폼에 아이디 또는 패스워드라고 적혀있다? 그럼 지워야지
+            if(loginForm.Text.Equals(loginForm.Tag.ToString()))
+            {
+                loginForm.Text = "";
+                loginForm.ForeColor = Color.Black;
+                    
+                //만약 비밀번호 텍스트 박스라면 마스킹 처리
+                if(loginForm.Tag.ToString().Equals("비밀번호"))
+                {
+                    loginForm.UseSystemPasswordChar = true;
+                }
+
+
+
+            }
+        }
+
+        private void loginForm_Leave(object sender, EventArgs e)
+        {
+            TextBox loginForm = sender as TextBox;
+
+            if(loginForm.Text.Equals(""))
+            {
+                loginForm.Text = loginForm.Tag.ToString();
+                loginForm.ForeColor = Color.Gray;
+
+                // 비밀번호 텍박이면 마스킹 해제 
+                if (loginForm.Tag.ToString().Equals("비밀번호"))
+                {
+                    loginForm.UseSystemPasswordChar = false;
+                }
+            }
+        }
+
+ 
+
+    }
+}
