4ddb726b43a15a23ae3c054a09e62163d0e7ec80
gimalgosa0514 <gmlakd990514@naver.com>
2025-08-05 22:37:51 +0900

    feat: Db수정한 버전 2 만드는 중

--- mealplan/domain/users/repository/UserRepositoryImpl.cs
+++ mealplan/domain/users/repository/UserRepositoryImpl.cs
@@ -56,11 +56,26 @@ namespace mealplan.domain.users.repository
             // 1. 커넥션을 얻어옴.
             using (OracleConnection conn = oracleUtil.GetConnection())
             {
-                string sql = $"SELECT * FROM SYS_SYSTEM_CODE_DATA_KHM WHERE CODE_NAME='{inputLoginId}' AND PLANT='MealPlan' AND TABLE_NAME='Users'";
+                string sql = $@"SELECT userId.DESCRIPTION, pwd.DESCRIPTION, name.DESCRIPTION, gender.DESCRIPTION, birthdate.DESCRIPTION, height.DESCRIPTION, weight.DESCRIPTION
+                                FROM SYS_SYSTEM_CODE_DATA_KHM userId
+                                    JOIN SYS_SYSTEM_CODE_DATA_KHM pwd
+                                        ON pwd.CODE_NAME = userId.CODE_NAME AND pwd.CODE_SEQ =2
+                                    JOIN SYS_SYSTEM_CODE_DATA_KHM name
+                                        ON name.CODE_NAME = userId.CODE_NAME AND name.CODE_SEQ = 3
+                                    JOIN SYS_SYSTEM_CODE_DATA_KHM gender
+                                        ON gender.CODE_NAME = userId.CODE_NAME AND gender.CODE_SEQ = 4
+                                    JOIN SYS_SYSTEM_CODE_DATA_KHM birthdate
+                                        ON birthdate.CODE_NAME = userId.CODE_NAME AND birthdate.CODE_SEQ=5
+                                    JOIN SYS_SYSTEM_CODE_DATA_KHM height
+                                        ON height.CODE_NAME = userId.CODE_NAME AND height.CODE_SEQ=6
+                                    JOIN SYS_SYSTEM_CODE_DATA_KHM weight
+                                        ON weight.CODE_NAME = userId.CODE_NAME AND weight.CODE_SEQ=7
+                                WHERE userId.PLANT='MealPlan2' AND userId.TABLE_NAME= 'Users' AND userId.CODE_NAME =:userId AND userId.CODE_SEQ = 1";
 
                 // 2.쿼리문 넣어서 OracleCommand 객체 맹글어줌.
                 using (OracleCommand oracleCommand = new OracleCommand(sql, conn))
-                {   
+                {
+                    oracleCommand.Parameters.Add("userId",inputLoginId);
                     // 결과는 이렇게 ResultSet처럼 끌어다 쓰면 됨
                     using (OracleDataReader result = oracleCommand.ExecuteReader())
                     {
@@ -70,13 +85,13 @@ namespace mealplan.domain.users.repository
                             return null;
                         }
 
-                        string loginId = result.GetString(2);
-                        string password = result.GetString(4);
-                        string name = result.GetString(5);
-                        string gender = result.GetString(6);
-                        string birthdate = result.GetString(7);
-                        string height = result.GetString(8);
-                        string weight = result.GetString(9);
+                        string loginId = result.GetString(0);
+                        string password = result.GetString(1);
+                        string name = result.GetString(2);
+                        string gender = result.GetString(3);
+                        string birthdate = result.GetString(4);
+                        string height = result.GetString(5);
+                        string weight = result.GetString(6);
 
                         return new User(loginId,password,name,gender,birthdate,height,weight);
                     }
@@ -93,50 +108,55 @@ namespace mealplan.domain.users.repository
             {
 
 
-                //새로운 유저를 넣기 전 마지막 MealPlan의 마지막 User의 code_seq를 보고 제일 마지막 seq + 1을 해주고 나서 넣어야함. 그래서 조회 먼저 해야함 ㅠ 
-                string selectSql = $@"SELECT CODE_SEQ 
-                                      FROM (SELECT CODE_SEQ FROM SYS_SYSTEM_CODE_DATA_KHM 
-                                            WHERE
-                                            PLANT = 'MealPlan' AND
-                                            TABLE_NAME = 'Users'
-                                            ORDER BY CODE_SEQ DESC)
-                                      WHERE ROWNUM = 1";
-
-                int lastSeq = 1;
-                using(OracleCommand cmd = new OracleCommand(selectSql, conn))
-                {
-                    using (OracleDataReader result = cmd.ExecuteReader())
-                    {
-                    
-                        // 만약 읽어지면 있는거니깐 lastSeq 갱신
-                        if(result.Read())
-                        {
-                            int savedLastSeq = int.Parse(result.GetString(0));
-                            lastSeq += savedLastSeq;
-                        }
-                        // 안읽히면? 1부터 시작하면 되니까 그대로 ㄱ
-                    }
-                }
+                //새 버전은 CODE_SEQ 찾는거 피룡없음.
               
                 // ORACLE에서는 VALUES 이런식으로 안하고, SELECT로 넣음
                 // 그리고 아이디는 중복될 수 없기 땜시롱... 제약조건을 걸 수는 없으니, 서브쿼리를 이용해서
                 // 아이디가 중복되지 않으면 들어갈 수 있도록 쿼리를 작성함.
-                string insertSql = $@"INSERT INTO SYS_SYSTEM_CODE_DATA_KHM 
-                (plant, table_name, code_name, code_seq, description, code_group1, code_group2, code_group3, code_group4, code_group5)
-                SELECT 'MealPlan', 'Users','{user.LoginId}','{lastSeq}', '{user.Password}','{user.Name}','{user.Birthdate}','{user.Gender}','{user.Height}','{user.Weight}'
-                FROM DUAL
-                WHERE NOT EXISTS
-                (SELECT CODE_NAME FROM SYS_SYSTEM_CODE_DATA_KHM WHERE CODE_NAME='{user.LoginId}')";
+                string insertSql = $@"INSERT ALL
+                                        INTO SYS_SYSTEM_CODE_DATA_KHM
+                                        VALUES ('MealPlan2','Users',:userId,1,:userId,null,null,null,null,null,null,null,null,null,null,null)
+                                        INTO SYS_SYSTEM_CODE_DATA_KHM
+                                        VALUES ('MealPlan2','Users',:userId,2,:pwd,null,null,null,null,null,null,null,null,null,null,null)
+                                        INTO SYS_SYSTEM_CODE_DATA_KHM
+                                        VALUES ('MealPlan2','Users',:userId,3,:name,null,null,null,null,null,null,null,null,null,null,null)
+                                        INTO SYS_SYSTEM_CODE_DATA_KHM
+                                        VALUES ('MealPlan2','Users',:userId,4,:gender,null,null,null,null,null,null,null,null,null,null,null)
+                                        INTO SYS_SYSTEM_CODE_DATA_KHM
+                                        VALUES ('MealPlan2','Users',:userId,5,:height,null,null,null,null,null,null,null,null,null,null,null)
+                                        INTO SYS_SYSTEM_CODE_DATA_KHM
+                                        VALUES ('MealPlan2','Users',:userId,6,:weight,null,null,null,null,null,null,null,null,null,null,null)
+                                        INTO SYS_SYSTEM_CODE_DATA_KHM
+                                        VALUES ('MealPlan2','Users',:userId,7,:birthdate,null,null,null,null,null,null,null,null,null,null,null)
+                                      SELECT * FROM DUAL
+                                        ";
                 using(OracleCommand cmd = new OracleCommand(insertSql, conn))
                 {
-                    if(cmd.ExecuteNonQuery() >= 1)
+                    // 디폴트가 순서 바인딩이라서, 이름 바인딩으로 바꿔주는거임.
+                    cmd.BindByName = true;
+                    cmd.Parameters.Add("userId", user.LoginId);
+                    cmd.Parameters.Add("pwd", user.Password);
+                    cmd.Parameters.Add("name", user.Name);
+                    cmd.Parameters.Add("gender", user.Gender);
+                    cmd.Parameters.Add("height", user.Height);
+                    cmd.Parameters.Add("weight", user.Weight);
+                    cmd.Parameters.Add("birthdate", user.Birthdate);
+                    try
                     {
-                        MessageBox.Show("회원가입 완료");
-                        return true;
-                    } else
+                        if (cmd.ExecuteNonQuery() >= 1)
+                        {
+                            MessageBox.Show("회원가입 완료");
+                            return true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("회원가입 실패 : 중복된 아이디입니다.");
+                        }
+                    } catch(OracleException ex)
                     {
-                        MessageBox.Show("회원가입 실패 : 중복된 아이디입니다.");
+                        MessageBox.Show("이미 중복된 계정입니다.");
                     }
+                    
                     
            
                 }
